5b3f586e1a7195e1028be8b302dae82cef24048c
Gabriel Moura <gabrieldossantosmoura2@gmail.com>
2025-05-14 15:50:16 -0300

    partições função

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,3 +7,22 @@ def load_image(name):
 def load_sound(name):
     path = f"assets/sounds/{name}.wav"
     return pygame.mixer.Sound(path)
+
+def generate_partitions(target_sum, max_parts):
+    partitions = []
+    
+    def backtrack(remaining_sum, max_value, current_partition, parts_left):
+        if remaining_sum == 0:
+            partitions.append(current_partition[:])
+            return
+        if parts_left == 0 or remaining_sum < 0:
+            return
+        
+        for value in range(max_value, 0, -1):
+            if value <= remaining_sum:
+                current_partition.append(value)
+                backtrack(remaining_sum - value, value, current_partition, parts_left - 1)
+                current_partition.pop()
+    
+    backtrack(target_sum, target_sum, [], max_parts)
+    return partitions
